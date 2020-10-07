934382f9d90cb9d4eb2f2bf38a53b20a3891e5c5
Попов Никита <barbaroso1346@gmail.com>
2020-10-07 12:08:14 +0700

    Initial Commit

---
+++ Walk on borders/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Walk on borders
+//
+//  Created by Никита Попов on 07.10.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
